2da9e60263dac0662478bc796333af7f6237b728
David Gereau <davidgereau@gmail.com>
2021-05-10 15:38:22 -0600

    Update SendConfirmation.php

--- AppData/Controllers/SendConfirmation.php
+++ AppData/Controllers/SendConfirmation.php
@@ -21,8 +21,8 @@ $mail->SMTPDebug = 2;
 $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true; 
-$mail->Username = 'appliedLanguageResearch@gmail.com'; 
-$mail->Password = 'meowmeow!123';
+$mail->Username = ''; 
+$mail->Password = '';
 $mail->SMTPSecure = 'tls';
 $mail->Port = 587;
 
